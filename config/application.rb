c4fc60e9c056a3ff5fa283b41ff4a3eaf4beffa3
Edem <keltikan@gmail.com>
2019-04-08 01:22:55 +0300

    RU locale

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module Squiggle
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'St. Petersburg'
+    config.i18n.default_locale = :ru
   end
 end
