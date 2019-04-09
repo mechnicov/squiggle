a29bb6ffdad15301f3c39ba0c4e65e6a179ba608
Edem <keltikan@gmail.com>
2019-04-09 13:59:30 +0300

    Only admin and owner can watch user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
+    unless current_user == @user || (current_user && current_user.is_admin)
+      flash[:alert] = I18n.t('not_authorized')
+      redirect_to root_url
+    end
   end
 end
