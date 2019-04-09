6832515f390b9e8d93898786a913e78aada18e8b
Edem <keltikan@gmail.com>
2019-04-10 00:40:44 +0300

    Fix password bug after reCAPTCHA adding

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, :password, presence: true
+
+  def update_with_password(params={})
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+    update_attributes(params)
+  end
 end
